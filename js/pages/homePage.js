d836fe6204adde2468c3cce64d7ac50e1db0630e
bhaumik55231 <bhaumik55231@gmail.com>
2020-06-02 02:43:06 -0400

    css update

--- js/pages/homePage.js
+++ js/pages/homePage.js
@@ -37,41 +37,53 @@ export const homePage = async () => {
             <div class="col"><img src="./images/sanford vector.png" class="site-logo"></div>
         </div>
         <div class="row">
-            <div class="col about-connect">
-                <span class="heading">Advancing Cancer Research</span> <i class="fab fa-searchengin"></i>
-                </br></br>
-                <span class="description">
-                    The Connect Study wants to better understand:
-                    <ul class="list">
-                        <li><strong>What causes cancer,</strong></li>
-                        <li><strong>Ways to prevent cancer, and</strong></li>
-                        <li><strong>How to improve early detection of cancer.</strong></li>
-                    </ul>
-                </span>
+            <div class="col-md-3 about-cancer">
+                <div class="row">
+                    <span class="heading">Advancing Cancer Research</span> <i class="fab fa-searchengin"></i>
+                </div>
+                <div class="row">
+                    <span class="description">
+                        The Connect Study wants to better understand:
+                        <ul class="list">
+                            <li><strong>What causes cancer,</strong></li>
+                            <li><strong>Ways to prevent cancer, and</strong></li>
+                            <li><strong>How to improve early detection of cancer.</strong></li>
+                        </ul>
+                    </span>
+                </div>
             </div>
-            <div class="col about-connect">
-                <span class="heading">Why should I join Connect?</span> <i class="fas fa-users"></i>
-                </br></br>
-                <span class="description">
-                    <strong>Being a part of Connect means you are contributing to the future of cancer prevention for our families and communities.</strong></br>
-                    With your help, Connect will be one of the largest and most important cancer studies in the United States.    
-                </span>
+            <div class="col-md-3 about-cancer">
+                <div class="row">
+                    <span class="heading">Why should I join Connect?</span> <i class="fas fa-users"></i>
+                </div>
+                <div class="row">
+                    <span class="description">
+                        <strong>Being a part of Connect means you are contributing to the future of cancer prevention for our families and communities.</strong></br>
+                        With your help, Connect will be one of the largest and most important cancer studies in the United States.    
+                    </span>
+                </div>
             </div>
-            <div class="col about-connect">
-                <span class="heading">Who can join Connect?</span> <i class="fas fa-female"></i><i class="fas fa-male"></i>
-                </br></br>
-                <span class="description">
-                    <ul class="list">
-                        <li><strong>Men and women between the ages of 40 and 65</strong></li>
-                        <li><strong>Current (patients/members) of participating sites</strong></li>
-                        <li><strong>No previous history of cancer</strong> (other than non-melanoma skin cancer)</li>
-                    </ul>
-                </span>
+            <div class="col-md-3 about-cancer">
+                <div class="row">
+                    <span class="heading">Who can join Connect?</span> <i class="fas fa-female"></i><i class="fas fa-male"></i>
+                </div>
+                <div class="row">
+                    <span class="description">
+                        <ul class="list">
+                            <li><strong>Men and women between the ages of 40 and 65</strong></li>
+                            <li><strong>Current (patients/members) of participating sites</strong></li>
+                            <li><strong>No previous history of cancer</strong> (other than non-melanoma skin cancer)</li>
+                        </ul>
+                    </span>
+                </div>
             </div>
-            <div class="col about-connect">
-                <span class="heading">Share Connect</span> <i class="fas fa-share"></i>
-                </br></br>
-                <img class="bar-code-image" src="./images/connectApp.png">
+            <div class="col-md-2">
+                <div class="row">
+                    <span class="heading">Share Connect</span> <i class="fas fa-share"></i>
+                </div>
+                <div class="row">
+                    <img class="bar-code-image" src="./images/connectApp.png">
+                </div>
             </div>
         </div>
     `;

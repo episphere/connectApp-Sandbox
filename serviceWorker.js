a8f25a17d853a972c614a9d10ba5fdd906a12ed0
bhaumik55231 <bhaumik55231@gmail.com>
2020-06-04 15:48:49 -0400

    updating html file version in sw

--- serviceWorker.js
+++ serviceWorker.js
@@ -68,4 +68,4 @@ registerRoute(
     'POST'
 );
 
-workbox.precaching.precacheAndRoute([{url: 'index.html', revision: '1591074620790'}]);
+workbox.precaching.precacheAndRoute([{url: 'index.html', revision: '1591300067984'}]);

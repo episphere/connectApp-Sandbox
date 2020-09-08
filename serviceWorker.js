2a78b32162ecc368bdaab41a2dd92aeb59f5a3e0
bhaumik55231 <bhaumik55231@gmail.com>
2020-09-08 00:16:13 -0400

    style updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -70,4 +70,4 @@ registerRoute(
     'POST'
 );
 
-workbox.precaching.precacheAndRoute([{url: 'index.html', revision: '9'}]);
+workbox.precaching.precacheAndRoute([{url: 'index.html', revision: '10'}]);

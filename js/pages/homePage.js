1eb23d54fa0fcf9e5acdc15e7de922bb928bcf3a
bhaumik55231 <bhaumik55231@gmail.com>
2020-09-07 23:50:14 -0400

    GUI updates

--- js/pages/homePage.js
+++ js/pages/homePage.js
@@ -5,67 +5,73 @@ export const homePage = async () => {
         ${isIE ? `<span class="not-compatible">Connect web application is not compatible with Internet Explorer, please use Chrome, Safari, Firefox or Edge</span>` : ``}
         <div class="row homepage-slider">
             <div class="col">
-                <img src="./images/newImages/4.PNG" height="400px" width="100%">
+                <img src="./images/newImages/4.PNG" height="100%" width="100%">
             </div>
         </div>
         <div class="row what-study-row">
-            <div class="col"><img src="./images/newImages/2.PNG" height="380px" width="440px"></div>
-            <div class="align-left d-flex justify-content-center align-items-center">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-4"><img src="./images/newImages/2.PNG" class="landing-page-images"></div>
+            <div class="col-lg-4 align-left d-flex justify-content-center align-items-center">
                 <div>
                     <span class="what-study-heading"><strong>What the study is</strong></span></br>
                     <span class="what-study-content">
-                        Connect for Cancer Prevention is one of the largest</br>
-                        and most important cancer studies in the United States.</br>
-                        By joining Connect, you will be a part of a community of researchers </br>
-                        and 200,000 people from around the country, who, like you, </br>
+                        Connect for Cancer Prevention is one of the largest
+                        and most important cancer studies in the United States.
+                        By joining Connect, you will be a part of a community of researchers
+                        and 200,000 people from around the country, who, like you,
                         want to contribute to cancer research.
                     </span>
                 </div>
             </div>
+            <div class="col-lg-2"></div>
         </div>
         <div class="how-study-row">
             <div class="row">
-                <div class="align-left d-flex justify-content-center align-items-center">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-4 align-left d-flex justify-content-center align-items-center">
                     <div>
                         <span class="how-study-heading"><strong>How you can help</strong></span></br>
                         <span class="how-study-content">
-                            While researchers have made important advances,</br>
-                            there is more to learn to ease the burden of cancer</br>
-                            on our families and our communities. Your </br>
-                            participation in this study will give scientist the </br>
-                            ability to look at how the way we live, our genes, </br>
+                            While researchers have made important advances,
+                            there is more to learn to ease the burden of cancer
+                            on our families and our communities. Your
+                            participation in this study will give scientist the
+                            ability to look at how the way we live, our genes,
                             and our medical history are connected to cancer.
                         </span>
                     </div>
                 </div>
-                <div class="col align-right"><img src="./images/newImages/3.PNG" height="380px" width="440px"></div>
+                <div class="col-lg-4 align-right"><img src="./images/newImages/3.PNG" class="landing-page-images"></div>
+                <div class="col-lg-2"></div>
             </div>
         </div>
         <div class="row who-can-join-heading" style="margin-top:30px">
             <div class="col align-center">Who can join</div>
         </div>
         <div class="row" style="margin:30px 0px">
-            <div class="col-md-2"></div>
-            <div class="col-md-1"><img src="./images/newImages/5.PNG" height="80px" width="100px"></div>
-            <div class="col-md-3">40-65 years old and </br> have <strong>never been diagnosed with </br> most kinds of invasive cancers</strong></div>
-            <div class="col-md-1"><img src="./images/newImages/6.PNG" height="80px" width="100px"></div>
-            <div class="col-md-3">Member or patient od </br> have <strong>participanting health care systems</strong></div>
-            <div class="col-md-2"></div>
+            <div class="col-lg-2"></div>
+            <div class="col-lg-1"><img src="./images/newImages/5.PNG" class="landing-page-images"></div>
+            <div class="col-lg-3">40-65 years old and </br> have <strong>never been diagnosed with </br> most kinds of invasive cancers</strong></div>
+            <div class="col-lg-1"><img src="./images/newImages/6.PNG" class="landing-page-images"></div>
+            <div class="col-lg-3">Member or patient od </br> have <strong>participanting health care systems</strong></div>
+            <div class="col-lg-2"></div>
         </div>
         <div class="learning-from-everyone">
             <div class="row">
-                <div class="col"><img src="./images/newImages/7.PNG" height="380px" width="440px"></div>
-                <div class="align-left d-flex justify-content-center align-items-center">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-4"><img src="./images/newImages/7.PNG" class="landing-page-images"></div>
+                <div class="col-lg-4 align-left d-flex justify-content-center align-items-center">
                     <div>
                         <span class="what-study-heading"><strong>Learning from everyone</strong></span></br>
                         <span class="what-study-content">
-                            Connect for Cancer Prevention will be most</br>
-                            successful if a diverse group of participants from</br>
-                            different environments and backgrounds join. This </br>
+                            Connect for Cancer Prevention will be most
+                            successful if a diverse group of participants from
+                            different environments and backgrounds join. This
                             allows all people to benefit from our discoveries.
                         </span>
                     </div>
                 </div>
+                <div class="col-lg-2"></div>
             </div>
         </div>
         <div class="participating-health-care-systems">
@@ -73,9 +79,9 @@ export const homePage = async () => {
                 <div class="col align-center">Participanting Healt Care Systems</div>
             </div>
             <div class="row who-can-join-heading" style="margin-top:30px">
-                <div class="col-md-1"></div>
-                <div class="col-md-10"><img src="./images/newImages/8.PNG" height="200px" width="100%"></div>
-                <div class="col-md-1"></div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-10"><img src="./images/newImages/8.PNG" class="landing-page-images"></div>
+                <div class="col-lg-1"></div>
             </div>
         </div>
 
